ef2ea2d8c51bedd64fa90879dfbf55fa7caf2429
Kenny Cordero <GeckoB4I@gmail.com>
2023-03-17 07:55:24 -0500

    fixed the date issue with the seeds

--- seeds/postSeeds.js
+++ seeds/postSeeds.js
@@ -4,14 +4,14 @@ const postsSeeds = [
     //Clay's post
     {
         user_id: 1, 
-        post_date: "01/01/2022",
+        post_date: "01/01/2018",
         post_title: "BioShock Infinite",
         post_content: "BioShot Infinite's story line is the best storyline in any video game!!"
     },
     //Mike's post
     {
         user_id: 2, 
-        post_date: "01/01/2022",
+        post_date: "01/01/2018",
         post_title: "Super Smash Bros. Melee",
         post_content: "Melee is better than ultimate!!"
     },
